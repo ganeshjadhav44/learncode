6df3139a048e4f3bd9a5c1f3d28be1c071488fcb
Ganesh Jadhav <ganesh.jadhav@cloudtravellers.co.in>
2018-10-23 07:35:31 -0700

    Add project files.

---
+++ mywebapp1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mywebapp1.Startup))]
+namespace mywebapp1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
